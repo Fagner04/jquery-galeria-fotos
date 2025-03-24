c3b2486041741f753b02c4ce73e3d42fb1c59daa
Fagner04 <Fagnerc04@gmail.com>
2025-03-24 00:58:23 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('header button').click(function () {
+        $('form').slideDown();
+    })
+
+    $('#button-cancelar').click(function () {
+        $('form').slideUp();
+    })
+
+    // // CALBACK EM JAVA-SCRIPT
+    // document.querySelector('header button').addEventListener('click', function() {
+
+    // })
+    // // CALBACK EM JQUARY
+    // $('header button').click(function() { 
+    //    alert('Olá mundo')
+
+    // })
+    //REMOVER SUBMIT DE ATUALIZAR A PAGINA
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none;"></li>');
+
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+           <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Abre imagem"> Ver imagem</a>
+           </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+    })
+})
